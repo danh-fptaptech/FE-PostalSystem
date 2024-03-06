8491ec374ecf91477391e6b623906cfd6fb80f37
Tran Vinh Phat <phattran1023@gmail.com>
2024-03-06 15:20:39 +0700

    Refactor tracking shipment page and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,28 +6,27 @@ import theme from '@/theme'
 import {Toaster} from 'sonner'
 import '@/styles/globals.css'
 import SessionProvider from '@/contexts/SessionProvider'
-import {SiteProvider} from "@/contexts/SiteContext'
+import {SiteProvider} from '@/contexts/SiteContext'
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-
-    return (
-        <SiteProvider>
-            <html lang="en">
-            <body>
-            <SessionProvider>
-                <AppRouterCacheProvider options={{enableCssLayer: true}}>
-                    <ThemeProvider theme={theme}>
-                        <CssBaseline/>
-                        {props.children}
-                    </ThemeProvider>
-                </AppRouterCacheProvider>
-            </SessionProvider>
-            <Toaster
-                closeButton
-                richColors
-            />
-            </body>
-            </html>
-        </SiteProvider>
-    );
+  return (
+    <SiteProvider>
+      <html lang='en'>
+        <body>
+          <SessionProvider>
+            <AppRouterCacheProvider options={{enableCssLayer: true}}>
+              <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                {props.children}
+              </ThemeProvider>
+            </AppRouterCacheProvider>
+          </SessionProvider>
+          <Toaster
+            closeButton
+            richColors
+          />
+        </body>
+      </html>
+    </SiteProvider>
+  )
 }
